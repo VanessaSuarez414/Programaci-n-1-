f84170dbbb40caea9e750c315273050eced9f4a9
Angie Suarez <angiesuarez125@unisangil.edu.co>
2025-09-01 19:14:47 -0500

    operadores Aritméticos

---
+++ operadoresaritmeticos.py
@@ -0,0 +1,17 @@
+#Operadores Aritméticos
+import math
+#Libreria
+
+#Ejemplo 1
+a =10 +2 (-3* -((7-2) + 7 * (3 -2)))
+print(a)
+#Ejemplo 2
+b = 2 *(4 *(7 +4  * (5 * 3 - 9)) - 3 + (40 - 8))
+print(f"Ejemplo 2={b}")
+#Ejemplo 3
+c = 3 * (4 +(2(3 + 5 ))) + 2
+print(c)
+
+#Ejemplo 5
+
+ 
