44bbf2274bb8576bfbcce0f8b2f921248f5f9cf1
Angie Suarez <angiesuarez125@unisangil.edu.co>
2025-09-03 14:56:49 -0500

    aplicacion verdadero falso

---
+++ Operadores_Comparaciones.py
@@ -0,0 +1,13 @@
+#Operadores de comparacion
+num1 = 5
+num2 = 10
+#Igualdad
+print(num1 == num2)
+#Difrencia
+print(num1 != num2)
+#Mayor que 
+print(num1 > num2)
+#Menor que
+print(num1 < num2 )
+
+
