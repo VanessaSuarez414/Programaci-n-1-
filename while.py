90dc3620f62fe7a020f9758e35d64bfb2d6b09b3
Angie Suarez <angiesuarez125@unisangil.edu.co>
2025-09-10 14:56:24 -0500

    while

---
+++ while.py
@@ -0,0 +1,14 @@
+#Estructura de repetición while
+op = int(input("Ingrese su opción: "))
+while op !=0:
+    print("Unisangil")
+print("Fuera de bucle")
+""""""
+# while true 
+while True:
+    numero = int(input("Ingrese su número: "))
+    if numero == 0:
+        break
+    else:
+        print(f"Error!!!")
+print("Fuera de bucle")
