4c385ce7cc049949983ef3842f92d556e1d7aee2
Angie Suarez <angiesuarez125@unisangil.edu.co>
2025-09-10 14:28:07 -0500

    ejemplo clase 10 de septiembre

---
+++ estructura.py
@@ -0,0 +1,13 @@
+#Estructura de control : if
+numero = 5
+#if
+if numero > 0:
+    print("El numero es positivo")
+# if - else 
+if numero > 0:
+    print("El numero es positivo")
+else:
+    print("El numero es negativo")
+#if-elif-else
+if numero == 1:
+    print("numero 1")
